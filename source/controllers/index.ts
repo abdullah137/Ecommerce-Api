77f80620401d4ec8ed25dee9e175626998863fe2
Abdullah <abdullahyahaya46@gmail.com>
2023-01-22 15:02:41 +0100

    Changed the mesasge from json

--- source/controllers/index.ts
+++ source/controllers/index.ts
@@ -2,19 +2,25 @@ import { Request, Response } from "express";
 
 const homePage = (req:Request, res:Response) => {
 	return res.status(200).json({
-		page: "Home page"
+		page: "Home page",
+		status: true,
+		message: "Welcome to the home page section"
 	});
 };
 
 const aboutPage = (req:Request, res:Response) => {
 	return res.status(200).json({
-		page: "About Page"
+		page: "About Page",
+		status: true,
+		message: "All Enquiry will be needed here"
 	});
 };
 
 const contactPage = (req:Request, res:Response) => {
 	return res.status(200).json({
-		page: "Contact Page"
+		page: "Contact Page",
+		status: true,
+		message: "All infomation will be needed too as well"
 	});
 };
 
