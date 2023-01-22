c1d3c2c1d1613d077254d908519d7891ad5a710e
Abdullah <abdullahyahaya46@gmail.com>
2023-01-22 15:03:00 +0100

    Created Two New Methods For Adding

--- source/controllers/cart/index.ts
+++ source/controllers/cart/index.ts
@@ -10,16 +10,29 @@ import logger from "../../utils/logging";
 import Carts from "../../models/cart";
 import Product from "../../models/products";
 
-type Item = {
-	items: Array<object>;
-    productId: string;
-    quantity: number;
-    price: number;
-    total: number;
-    subTotal: number;
-	id: string
+
+interface ArrayProduct {
+	productId: {
+		id: string,
+		name: string,
+		price: number,
+	},
+	quantity: number,
+	price: number,
+	total: number,
+	_id: string,
+	updatedAt: Date,
+	createdAt: Date
 }
 
+interface Testing {
+	_id: string;
+	items: ArrayProduct[],
+	subTotal: number,
+	createdAt: Date,
+	updatedAt: Date,
+	__v: number;
+}
 
 const addToCart = async (req: Request, res:Response) => {
 	const data = res.locals.data as Cart;
@@ -37,39 +50,68 @@ const addToCart = async (req: Request, res:Response) => {
 
 		if(!productDetails) {
 			return res.status(500).json({
-				type: "Not Found",
+				error: "NOT_FOUND",
 				status: false,
 				msg: "Sorry, The Product you just entered do not exist"
 			});
 		}
-
-
 
 		const cart = await Carts.find().populate({
 			path: "items.productId",
 			select: "name price total"
 		});
 
-
-		//if cart exists
-		if(cart) {
-			const indexFound = cart.findIndex((item:Item) =>  item.productId == productId);
-
-			if (indexFound !== -1 && quantity <= 0) {
-				cart.splice(indexFound, -1);
-
-			
+		console.log(cart);
+		console.log(cart[0].items);
+		
+		if(cart.length > 0) {
+			// Check if index exists
+			const indexFound = cart[0].items.findIndex((item: any) => item.productId.id == productId);
+			// ---- This removed an item from the cart if found any
+			if(indexFound !== -1 && Number(quantity) <= 0) {
+				cart[0].items.splice(indexFound, 1);
 			}
+
+
+			if(cart[0].items.length == 0) {
+				cart[0].subTotal = 0;
+			} else {
+				cart[0].subTotal = cart[0].items.map((item: any) => item?.total).reduce((acc, next) => acc + next);
+			}
+
+		} else if(quantity > 0) {
+			cart[0].items.push({
+				productId: productId,
+				quanity: quantity,
+				price: productDetails?.price,
+				total: productDetails?.price * quantity
+			});
+			cart[0].subTotal = cart[0].items.map((item: any) => item?.total).reduce((acc, next) => acc + next);
+		}else {
+
+			console.log("It is here 2");
 			
-		
+			const cartData = {
+				items: [{
+					productId: productId,
+					quantity: quantity,
+					total:productDetails?.price * quantity,
+					price: productDetails?.price
+				}],
+				subTotal: (productDetails?.price * quantity)
+			};
 
+			// Adding Section to cart
+			const cart = await Carts.create(cartData);
 
-		} else {
-			console.log("It is here");
+			return res.status(200).json({
+				query: "SUCCESS",
+				status: true,
+				data: cart
+			});
 		}
-
-		//----------check if product exist,just add the previous quantity with the new quantity and update the total price-------
 		
+	
 	} catch(err:any) {
 		return res.status(500).json({
 			error: "INTERNAL_ERROR",
@@ -80,5 +122,57 @@ const addToCart = async (req: Request, res:Response) => {
 };
 
 
+const getCart = async(req: Request, res: Response) => {
+	try {
+		const cart = await Carts.find().populate({
+			path: "items.productId",
+			select: "name price total"
+		});
+		
+		if(!cart) {
+			return res.status(404).json({
+				error: "CART_EMPTY",
+				status: true,
+				message: "Oops, you cart is empty."
+			});
+		}
 
-export default { addToCart };
+		return res.status(200).json({
+			message: "CART_LISTS",
+			status: true,
+			products: cart
+		});
+
+	}catch(error: any) {
+		logger.error(error);
+		return res.status(500).json({
+			error: "INTERNAL_ERROR",
+			status: false,
+			message: error.message
+		});
+	}
+};
+
+
+const emptyCart = async(req: Request, res: Response) => {
+	try {
+		const cart = await Carts.find();
+
+		return res.status(200).json({
+			message: "QUERY_SUCCESS",
+			status: true,
+			data: cart
+		});
+	}catch(error: any) {
+		logger.error(error);
+		return res.status(400).json({
+			error: "INTERNAL_ERROR",
+			status: false,
+			message: "Some internal error occured",
+		});
+	}
+};
+
+
+
+export default { addToCart, getCart, emptyCart };
